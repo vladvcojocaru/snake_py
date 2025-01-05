bbef457c13709ab22594d2c2f0b83c48151c5f8f
vladvcojocaru <vladvcojocaru@gmail.com>
2025-01-05 12:18:12 +0200

    game finished

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import random
 import sys
 import pygame
+from pygame.constants import SRCCOLORKEY
 from pygame.math import Vector2
 
 
@@ -83,6 +84,7 @@ class Snake:
         if tail_relation == Vector2(0, 1): self.tail = self.tail_up
         if tail_relation == Vector2(0, -1): self.tail = self.tail_down
 
+
     def _update_head_graphics(self):
         head_relation = self.body[1] - self.body[0]
         if head_relation == Vector2(1, 0): self.head = self.head_left
@@ -107,7 +109,7 @@ class Snake:
 
 
 class Main:
-    def __init__(self) -> None:
+    def __init__(self):
         self.snake = Snake()
         self.fruit = Fruit()
 
@@ -120,6 +122,7 @@ class Main:
         self.draw_grass()
         self.fruit.draw_fruit()
         self.snake.draw_snake()
+        self.draw_score()
 
     def _check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
@@ -157,6 +160,19 @@ class Main:
                         grass_rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
                         pygame.draw.rect(screen, grass_color, grass_rect)
 
+    def draw_score(self):
+        score_text = str(len(self.snake.body) - 3)
+        score_surface = game_font.render(score_text, True, (56, 74, 12))
+        score_x = int(cell_size * cell_number - 60)
+        score_y = int(cell_size * cell_number - 40)
+        score_rect = score_surface.get_rect(center = (score_x, score_y))
+        apple_rect = apple.get_rect(midright = (score_rect.left, score_rect.centery))
+
+
+        screen.blit(score_surface, score_rect)
+        screen.blit(apple, apple_rect)
+
+
 
 
 pygame.init()
@@ -165,6 +181,7 @@ cell_number = 20
 screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number))
 clock = pygame.time.Clock()
 apple = pygame.image.load("graphics/apple.png").convert_alpha()
+game_font = pygame.font.Font("fonts/FiraCodeNerdFont-Regular.ttf", 25)
 
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 150)
